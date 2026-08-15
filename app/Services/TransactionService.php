a74506d1f95e022d2897dbae1328d2e23ffc45ff
Reza <reza_pb71@yahoo.com>
2026-08-15 21:37:21 +0700

    change redirect after login

--- app/Services/TransactionService.php
+++ app/Services/TransactionService.php
@@ -9,7 +9,6 @@ class TransactionService
 {
     public function index(Request $request)
     {
-        $queryTransaksi = Transaction::query();
 
         $qdays = 0;
 
@@ -23,23 +22,22 @@ class TransactionService
             };
         }
 
-        $queryTransaksi
-            ->when(
-                $request->missing('days'),
-                fn($query) => $query->whereDate('created_at', '=', today())->orderBy('created_at', 'desc'), // tidak ada days
-                fn($query) => $query->whereDate('created_at', '>=', now()->subDays((int) $qdays))->orderBy('created_at', 'asc') // ada days
-            )->when(
-                $request->filled('search'),
-                fn($query) => $query
-                    ->where(
-                        fn($q) => $q   // grouping query, karena ada orWhereHas
-                            ->where('nominal', 'like', '%' . $request->search . '%')
-                            ->orWhereHas('masterBank', fn($q2) => $q2->where('name', 'like', '%' . $request->search . '%'))
-                    )
-            );
+        $transaksi = Transaction::when(
+            $request->missing('days'),
+            fn($query) => $query->whereDate('created_at', '=', today())->orderBy('created_at', 'desc'), // tidak ada days
+            fn($query) => $query->whereDate('created_at', '>=', now()->subDays((int) $qdays))->orderBy('created_at', 'asc') // ada days
+        )->when(
+            $request->filled('search'),
+            fn($query) => $query
+                ->where(
+                    fn($q) => $q   // grouping query, karena ada orWhereHas
+                        ->where('nominal', 'like', '%' . $request->search . '%')
+                        ->orWhereHas('masterBank', fn($q2) => $q2->where('name', 'like', '%' . $request->search . '%'))
+                )
+        )->paginate(20)->withQueryString();
 
 
-        $transaksi = $queryTransaksi->paginate(20)->withQueryString();
+
 
         return $transaksi;
     }
